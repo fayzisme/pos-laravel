17ea7ae1483842cabaeabb1ca78056c555ef8bdc
yusuf <hafidhy288@gmail.com>
2024-04-17 17:13:31 +0700

    refactor : sidebar

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -1,91 +1,83 @@
 <script setup>
-    import { onMounted, ref, computed, watch } from 'vue';
-    import { Link } from '@inertiajs-fix-scroll/vue3';
+import { onMounted, ref, computed, watch } from 'vue';
+import { Link } from '@inertiajs-fix-scroll/vue3';
+const logo = window._asset + `assets/dist/img/AdminLTELogo.png`;
+const avatar = ref(window._asset + `assets/dist/img/user2-160x160.jpg`);
 
-    const logo = window._asset + `assets/dist/img/AdminLTELogo.png`;
-    const avatar = ref(window._asset + `assets/dist/img/user2-160x160.jpg`);
+// Membuat computed property untuk menghasilkan kondisi sidebar
+const isSidebarCollapsed = computed(() => {
+   return !$('.sidebar-collapse').length < 1;
+});
 
-    // Membuat computed property untuk menghasilkan kondisi sidebar
-    const isSidebarCollapsed = computed(() => {
-      return !$('.sidebar-collapse').length < 1;
-    });
+const _route = computed(() => {
+   return route().current();
+});
 
-    const _route = computed(() => {
-      return route().current();
-    });
+onMounted(() => {
+   console.log(route().current());
+})
 
-    onMounted(()=>{
-      console.log(route().current());
-    })
-    
 </script>
 
 <style scoped>
 .inactive {
-  display: none;
+   display: none;
 }
 </style>
 <template>
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <!-- <img :src="logo" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
-      <span class="brand-text font-weight-light">Point of Sale</span>
-    </a>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <!-- <div class="image">
-                <img :src="avatar" class="img-circle elevation-2" alt="User Image">
-            </div> -->
-            <div class="info" :class="{ inactive: isSidebarCollapsed }">
-                <a href="#" class="d-block">{{  $page.props.auth.user.name }}</a>
-            </div>
-        </div>
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <li class="nav-item">
-            <a :href="route('dashboard')" class="nav-link" :class="[(_route == 'dashboard') ? 'active' : '']">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a :href="route('orders.index')" class="nav-link" :class="[_route.includes('orders') ? 'active' : '']">
-              <i class="nav-icon fas fa-shopping-cart"></i>
-              <p>
-                Order
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a :href="route('produks.index')" class="nav-link" :class="[_route.includes('produks') ? 'active' : '']">
-              <i class="nav-icon fas fa-table"></i>
-              <p>
-                Produk
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a :href="route('transaksis.index')" class="nav-link" :class="[_route.includes('transaksis') ? 'active' : '']">
-              <i class="nav-icon fas fa-table"></i>
-              <p>
-                Transaksi
-              </p>
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
-  </template>
+   <aside id="default-sidebar"
+      class="bg-white fixed top-0 left-0 z-40 w-62 h-screen transition-transform -translate-x-full sm:translate-x-0"
+      aria-label="Sidebar">
+      <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+         <ul class="space-y-2 font-medium mx-6 flex flex-col gap-4 nonactive ">
+            <li class="flex flex-col items-center gap-2 ">
+               <a href="">
+                  <img class="w-20 h-20 rounded-full"
+                     src="https://cdn0-production-images-kly.akamaized.net/n17EE2hpje18SmgwTh0JIrlCNiA=/640x853/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3960331/original/039270200_1647052633-IMG-20171112-WA0024.jpg"
+                     alt="Jese Leos">
+               </a>
+               <p class="text-base font-semibold leading-none text-gray-900 dark:text-white">
+               <div class="info" :class="{ inactive: isSidebarCollapsed }">
+                  <a href="#" class="d-block">{{ $page.props.auth.user.name }}</a>
+               </div>
+               </p>
+               <p class="text-sm font-normal">
+                  <a href="#" class="hover:underline">Waiter 4.h 53m</a>
+               </p>
+               <button type="button" class="btn btn-outline-secondary py-2 border-gray-200 text-sm">Open
+                  Profile</button>
+            </li>
+            <li>
+               <a :href="route('dashboard')" :class="[(_route == 'dashboard') ? 'active ' : '']"
+                  class="inline-flex items-center px-5 p-2 w-full text-gray-300 rounded-lg group">
+                  <i class="text-lg bi bi-ui-checks-grid"></i>
+                  <span class="ms-3 text-base font-semibold">Dashboard</span>
+               </a>
+            </li>
+            <li>
+               <a :href="route('orders.index')" :class="[_route.includes('orders') ? 'active' : '']"
+                  class="inline-flex items-center px-5 p-2 w-full text-gray-300 rounded-lg group">
+                  <i class="text-lg bi bi-cart3"></i>
+                  <span class="ms-3 text-base font-semibold"> Order</span>
+               </a>
+            </li>
+            <li>
+               <a :href="route('transaksis.index')" :class="[_route.includes('transaksis') ? 'active' : '']"
+                  class="inline-flex items-center px-5 p-2 w-full text-gray-300 rounded-lg group">
+                  <i class="text-lg bi bi-credit-card"></i>
+                  <span class="ms-3 text-base font-semibold">Transaksi</span>
+               </a>
+            </li>
+            <li>
+               <a :href="route('produks.index')" :class="[_route.includes('produks') ? 'active' : '']"
+                  class="inline-flex items-center px-5 p-2 w-full text-gray-300 rounded-lg group">
+                  <i class="text-lg bi bi-journal-text"></i>
+                  <span class="ms-3 text-base font-semibold"> Produk</span>
+               </a>
+            </li>
+         </ul>
+      </div>
+   </aside>
 
-
-  
+</template>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,137 +1,143 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        {{-- <link rel="preconnect" href="https://fonts.bunny.net"> --}}
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link
-        href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;1,700&display=swap"
-        rel="stylesheet"
-        />
-        <link
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        rel="stylesheet"
-        />
-        
-        {{-- Admin LTE --}}
-        <link rel="stylesheet" href="{{ asset ('assets/plugins/fontawesome-free/css/all.min.css')}}">
-        <!-- icheck bootstrap -->
-        <link rel="stylesheet" href="{{ asset ('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
-        <!-- Theme style -->
-        <link rel="stylesheet" href="{{ asset ('assets/dist/css/adminlte.min.css')}}">
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Ionicons -->
-        <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-        <!-- Tempusdominus Bootstrap 4 -->
-        <link rel="stylesheet" href="{{ asset ('assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
-        <!-- JQVMap -->
-        <link rel="stylesheet" href="{{ asset ('assets/plugins/jqvmap/jqvmap.min.css')}}">
-        <!-- overlayScrollbars -->
-        <link rel="stylesheet" href="{{ asset ('assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
-        <!-- Daterange picker -->
-        <link rel="stylesheet" href="{{ asset ('assets/plugins/daterangepicker/daterangepicker.css')}}">
-        <!-- summernote -->
-        <link rel="stylesheet" href="{{ asset ('assets/plugins/summernote/summernote-bs4.min.css')}}">
+    <!-- Fonts -->
+    {{-- <link rel="preconnect" href="https://fonts.bunny.net"> --}}
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;1,700&display=swap"
+        rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
 
-        <!-- iCheck for checkboxes and radio inputs -->
-        <link rel="stylesheet" href="{{  asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
-        <!-- Select2 -->
-        <link rel="stylesheet" href="{{  asset('assets/plugins/select2/css/select2.min.css')}}">
-        <link rel="stylesheet" href="{{  asset('assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css')}}">
-        <!-- Bootstrap4 Duallistbox -->
-        <link rel="stylesheet" href="{{  asset('assets/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css')}}">
-        <!-- BS Stepper -->
-        <link rel="stylesheet" href="{{  asset('assets/plugins/bs-stepper/css/bs-stepper.min.css')}}">
-        <!-- dropzonejs -->
-        <link rel="stylesheet" href="{{  asset('assets/plugins/dropzone/min/dropzone.min.css')}}">
+    {{-- Admin LTE --}}
+    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('assets/dist/css/adminlte.min.css') }}">
 
-        
-        <link rel="stylesheet" href="{{ asset ('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
-        <link rel="stylesheet" href="{{ asset ('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet"
+        href="{{ asset('assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/jqvmap/jqvmap.min.css') }}">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/daterangepicker/daterangepicker.css') }}">
+    <!-- summernote -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/summernote/summernote-bs4.min.css') }}">
 
-        <!-- Font Awesome -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+    <!-- iCheck for checkboxes and radio inputs -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- Select2 -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/select2/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
+    <!-- Bootstrap4 Duallistbox -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css') }}">
+    <!-- BS Stepper -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/bs-stepper/css/bs-stepper.min.css') }}">
+    <!-- dropzonejs -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/dropzone/min/dropzone.min.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
 
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+        integrity="sha512-f0gbBgNEbH4ZKZSy+1SbQfQdZh16knbIv8fB8+XT37vEy+RUUfNbGPDz0+nzPpLOw9HPIGufHZ4lszJaj7/HsQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/css/app.css','resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased hold-transition sidebar-mini">
-        @inertia
+    <!-- Scripts -->
+    @routes
+    @vite(['resources/css/app.css', 'resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+    @inertiaHead
+</head>
 
-        <script>
-            window._asset = '{{ asset('') }}';
-        </script>
+<body class="font-sans antialiased hold-transition sidebar-mini">
+    @inertia
 
-        <script src="{{ asset ('assets/plugins/jquery/jquery.min.js')}}"></script>
-    
-        <script src="{{ asset ('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-        
-        <script src="{{ asset ('assets/dist/js/adminlte.min.js')}}"></script>
+    <script>
+        window._asset = '{{ asset('') }}';
+    </script>
 
-        <!-- jQuery UI 1.11.4 -->
-        <script src="{{ asset ('assets/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
-        <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-        <script>
+    <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
+
+    <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <script src="{{ asset('assets/dist/js/adminlte.min.js') }}"></script>
+
+    <!-- jQuery UI 1.11.4 -->
+    <script src="{{ asset('assets/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    <script>
         $.widget.bridge('uibutton', $.ui.button)
-        </script>
-        <!-- ChartJS -->
-        <script src="{{ asset ('assets/plugins/chart.js/Chart.min.js')}}"></script>
-        <!-- Sparkline -->
-        <script src="{{ asset ('assets/plugins/sparklines/sparkline.js')}}"></script>
-        <!-- JQVMap -->
-        <script src="{{ asset ('assets/plugins/jqvmap/jquery.vmap.min.js')}}"></script>
-        {{-- <script src="{{ asset ('assets/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script> --}}
-        <!-- jQuery Knob Chart -->
-        <script src="{{ asset ('assets/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
-        <!-- daterangepicker -->
-        <script src="{{ asset ('assets/plugins/moment/moment.min.js')}}"></script>
-        <script src="{{ asset ('assets/plugins/daterangepicker/daterangepicker.js')}}"></script>
-        <!-- Tempusdominus Bootstrap 4 -->
-        <script src="{{ asset ('assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
-        <!-- Summernote -->
-        <script src="{{ asset ('assets/plugins/summernote/summernote-bs4.min.js')}}"></script>
-        <!-- overlayScrollbars -->
-        <script src="{{ asset ('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
-        <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-        <script src="{{ asset ('assets/dist/js/pages/dashboard.js')}}"></script>
+    </script>
+    {{-- bs-icon --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    {{-- ionicon --}}
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <!-- ChartJS -->
+    <script src="{{ asset('assets/plugins/chart.js/Chart.min.js') }}"></script>
+    <!-- Sparkline -->
+    <script src="{{ asset('assets/plugins/sparklines/sparkline.js') }}"></script>
+    <!-- JQVMap -->
+    <script src="{{ asset('assets/plugins/jqvmap/jquery.vmap.min.js') }}"></script>
+    {{-- <script src="{{ asset ('assets/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script> --}}
+    <!-- jQuery Knob Chart -->
+    <script src="{{ asset('assets/plugins/jquery-knob/jquery.knob.min.js') }}"></script>
+    <!-- daterangepicker -->
+    <script src="{{ asset('assets/plugins/moment/moment.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/daterangepicker/daterangepicker.js') }}"></script>
+    <!-- Tempusdominus Bootstrap 4 -->
+    <script src="{{ asset('assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
+    <!-- Summernote -->
+    <script src="{{ asset('assets/plugins/summernote/summernote-bs4.min.js') }}"></script>
+    <!-- overlayScrollbars -->
+    <script src="{{ asset('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="{{ asset('assets/dist/js/pages/dashboard.js') }}"></script>
 
-        <!-- Select2 -->
-        <script src="{{ asset('assets/plugins/select2/js/select2.full.min.js')}}"></script>
-        <!-- Bootstrap4 Duallistbox -->
-        <script src="{{ asset('assets/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js')}}"></script>
-        <!-- InputMask -->
-        <script src="{{ asset('assets/plugins/moment/moment.min.js')}}"></script>
-        <script src="{{ asset('assets/plugins/inputmask/jquery.inputmask.min.js')}}"></script>
-        <!-- date-range-picker -->
-        <script src="{{ asset('assets/plugins/daterangepicker/daterangepicker.js')}}"></script>
+    <!-- Select2 -->
+    <script src="{{ asset('assets/plugins/select2/js/select2.full.min.js') }}"></script>
+    <!-- Bootstrap4 Duallistbox -->
+    <script src="{{ asset('assets/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js') }}"></script>
+    <!-- InputMask -->
+    <script src="{{ asset('assets/plugins/moment/moment.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/inputmask/jquery.inputmask.min.js') }}"></script>
+    <!-- date-range-picker -->
+    <script src="{{ asset('assets/plugins/daterangepicker/daterangepicker.js') }}"></script>
 
-        <!-- Bootstrap Switch -->
-        <script src="{{ asset('assets/plugins/bootstrap-switch/js/bootstrap-switch.min.js')}}"></script>
-        <!-- BS-Stepper -->
-        <script src="{{ asset('assets/plugins/bs-stepper/js/bs-stepper.min.js')}}"></script>
-        <!-- dropzonejs -->
-        <script src="{{ asset('assets/plugins/dropzone/min/dropzone.min.js')}}"></script>
+    <!-- Bootstrap Switch -->
+    <script src="{{ asset('assets/plugins/bootstrap-switch/js/bootstrap-switch.min.js') }}"></script>
+    <!-- BS-Stepper -->
+    <script src="{{ asset('assets/plugins/bs-stepper/js/bs-stepper.min.js') }}"></script>
+    <!-- dropzonejs -->
+    <script src="{{ asset('assets/plugins/dropzone/min/dropzone.min.js') }}"></script>
 
-        <!-- DataTables  & Plugins -->
-        <script src="{{ asset ('assets/plugins/datatables/jquery.dataTables.min.js')}}"></script>
-        <script src="{{ asset ('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
-        <script src="{{ asset ('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
-        <script src="{{ asset ('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
-        <script src="{{ asset ('assets/plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
-        <script src="{{ asset ('assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
-        <script src="{{ asset ('assets/plugins/jszip/jszip.min.js')}}"></script>
-        <script src="{{ asset ('assets/plugins/pdfmake/pdfmake.min.js')}}"></script>
-        <script src="{{ asset ('assets/plugins/pdfmake/vfs_fonts.js')}}"></script>
-        <script src="{{ asset ('assets/plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
-        <script src="{{ asset ('assets/plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
-        <script src="{{ asset ('assets/plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
-    </body>
+    <!-- DataTables  & Plugins -->
+    <script src="{{ asset('assets/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+</body>
+
 </html>
